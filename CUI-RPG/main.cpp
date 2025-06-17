5bd130be7863904e5a7018f8788ed8c71243df96
shunsuke5 <shunsuke1359@gmail.com>
2025-06-17 13:35:43 +0900

    IsDie()を修正

--- CUI-RPG/main.cpp
+++ CUI-RPG/main.cpp
@@ -8,28 +8,11 @@
 #include <vector>
 using namespace std;
 
-class A
-{
-public:
-    virtual void a() { cout << "A" << endl; }
-};
-
-class B : public A
-{
-    void a() { cout << "B" << endl; }
-};
-
 int main()
 {
-    A* b = new B();
-    b->a();
-
-    A* a = new A();
-    a->a();
-
     BattleCharacter* brave = new Brave("kamei", 10, 5, 5, 5, 5);
     BattleCharacter* enemy = new Enemy("enemy", 7, 4, 3, 3, 8);
-    StateOperation::SetState(brave, State::POISON);
+    //StateOperation::SetState(brave, State::POISON);
 
     while (!brave->IsDie() && !enemy->IsDie()) {
         if (brave->IsFaster(enemy)) {
